59bf32fd4618f3514db14fe76f263de6352d6be3
sked1985 <mcgettiganbarry@yahoo.com>
2015-06-06 19:49:57 +0100

    Added gallery
    I added an image gallery to the application.

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ion-gallery'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -71,7 +71,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     views: {
       'menuContent': {
         templateUrl: "templates/leinster.html",
-        controller: 'LeinsterCtrl'
+        controller: 'ProvincesCtrl'
       }
     }
   })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -37,6 +37,8 @@ angular.module('starter.controllers', [])
 
 .controller('HomeCtrl', function($scope, $ionicModal) {
 
+  $scope.description="  Welcome to the dentist appointment scheduler. Select your location and make an appointment"
+
   $scope.openAbout = function(){
     $ionicModal.fromTemplateUrl('templates/about.html', {
       scope: $scope,
@@ -73,7 +75,7 @@ angular.module('starter.controllers', [])
   ];
 })
 
-.controller('LeinsterCtrl', function($scope, $stateParams, $ionicLoading, $timeout) {
+.controller('ProvincesCtrl', function($scope, $stateParams, $ionicLoading, $timeout) {
 
     $ionicLoading.show({
       content: 'Loading',
@@ -124,4 +126,68 @@ angular.module('starter.controllers', [])
 
 
 
+})
+
+.controller('ImageGalleryCtrl', function($scope){
+  $scope.items = [
+  {
+    src:'http://www.wired.com/images_blogs/rawfile/2013/11/offset_WaterHouseMarineImages_62652-2-660x440.jpg',
+    sub: 'This is a <b>subtitle</b>'
+  },
+  {
+    src:'http://www.wired.com/images_blogs/rawfile/2013/11/offset_WaterHouseMarineImages_62652-2-660x440.jpg',
+    sub: 'This is a <b>subtitle</b>'
+  },
+  {
+    src:'http://www.wired.com/images_blogs/rawfile/2013/11/offset_WaterHouseMarineImages_62652-2-660x440.jpg',
+    sub: 'This is a <b>subtitle</b>'
+  },
+  {
+    src:'http://www.wired.com/images_blogs/rawfile/2013/11/offset_WaterHouseMarineImages_62652-2-660x440.jpg',
+    sub: 'This is a <b>subtitle</b>'
+  },
+  {
+    src:'http://www.wired.com/images_blogs/rawfile/2013/11/offset_WaterHouseMarineImages_62652-2-660x440.jpg',
+    sub: 'This is a <b>subtitle</b>'
+  },
+  {
+    src:'http://www.wired.com/images_blogs/rawfile/2013/11/offset_WaterHouseMarineImages_62652-2-660x440.jpg',
+    sub: 'This is a <b>subtitle</b>'
+  },
+  {
+    src:'http://www.wired.com/images_blogs/rawfile/2013/11/offset_WaterHouseMarineImages_62652-2-660x440.jpg',
+    sub: 'This is a <b>subtitle</b>'
+  },
+  {
+    src:'http://www.wired.com/images_blogs/rawfile/2013/11/offset_WaterHouseMarineImages_62652-2-660x440.jpg',
+    sub: 'This is a <b>subtitle</b>'
+  },
+  {
+    src:'http://www.wired.com/images_blogs/rawfile/2013/11/offset_WaterHouseMarineImages_62652-2-660x440.jpg',
+    sub: 'This is a <b>subtitle</b>'
+  },
+  {
+    src:'http://www.wired.com/images_blogs/rawfile/2013/11/offset_WaterHouseMarineImages_62652-2-660x440.jpg',
+    sub: 'This is a <b>subtitle</b>'
+  },
+  {
+    src:'http://www.wired.com/images_blogs/rawfile/2013/11/offset_WaterHouseMarineImages_62652-2-660x440.jpg',
+    sub: 'This is a <b>subtitle</b>'
+  },
+  {
+    src:'http://www.wired.com/images_blogs/rawfile/2013/11/offset_WaterHouseMarineImages_62652-2-660x440.jpg',
+    sub: 'This is a <b>subtitle</b>'
+  },
+  {
+    src:'http://www.wired.com/images_blogs/rawfile/2013/11/offset_WaterHouseMarineImages_62652-2-660x440.jpg',
+    sub: 'This is a <b>subtitle</b>'
+  },
+  {
+    src:'http://www.gettyimages.co.uk/CMS/StaticContent/1391099215267_hero2.jpg',
+    sub: '' /* Not showed */
+  },
+  {
+    src:'http://www.hdwallpapersimages.com/wp-content/uploads/2014/01/Winter-Tiger-Wild-Cat-Images.jpg'
+  }
+]
 });
